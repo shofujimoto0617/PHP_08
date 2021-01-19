a9e141062ab4cf6510f43c10bc01de9f9d4644b3
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-19 17:55:20 +0900

    Add Admin and User to Login

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 
 class AdminFactory extends Factory
@@ -25,9 +26,9 @@ class AdminFactory extends Factory
     {
         return [
             'name' => 'Admin',
-            'email' => 'admin@gmail.com',
+            'email' => '12345@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('12345'), // password
             'remember_token' => Str::random(10),
         ];
     }
